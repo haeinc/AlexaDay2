8e1c734621baff6133a088a897a77f9a420c95e1
haeinc <62230101+haeinc@users.noreply.github.com>
2022-07-13 09:13:48 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+'''
+userName = input("Enter your name: ")
+print("Hey " + userName)
+
+userGrade = input("What grade are you in? : ")
+print("Hey " + userName + ", " + userGrade + " is a cool grade.")
+'''
+
+userYear = int(input("What year were you born? : "))
+userAge = 2022 - userYear
+print(str(userAge) + " is so young!")
